14e15a042ddcc9145222c17891f6a48951e9f11f
EllisJLC <jia.liang.chen@ryerson.ca>
2022-11-02 18:52:27 -0400

    final edits for comments

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -1,24 +1,26 @@
-const Pet = require('../../models/Pet');
-const cloudinary = require('cloudinary').v2;
-const user = require('../../models/User');
-const router = require('express').Router();
-const uploader = require('../../utils/helpers');
 
-cloudinary.config({
+const Pet = require("../../modelsxx/Pet");
+const cloudinary = require("cloudinary").v2;
+const user = require("../../modelsxx/User");
+const router = require("express").Router();
+const uploader = require("../../utils/helpers");
+
+
+cloudinary.config({ // Configure Cloudinary
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
-  secure:true,
+  secure: true,
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => { // Post new pet route
   try {
     let petForm = req.body;
-    const image = await uploader(req.body.image);
-    petForm.image = image.secure_url,
-    petForm.image_public_id = image.public_id
+    const image = await uploader(req.body.image); // Upload image to cloudinary, store response as image variable.
+    (petForm.image = image.secure_url), // set values of petForm to include image url for storage.
+    (petForm.image_public_id = image.public_id);
 
-    const newPet = await Pet.create({
+    const newPet = await Pet.create({ // Create new pet object
       ...req.body,
       user_id: req.session.user_id,
     });
@@ -28,14 +30,13 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.get('/', async (req,res) => {
+router.get("/", async (req, res) => {
   try {
-    const pets = await Pet.findAll({})
+    const pets = await Pet.findAll({});
     res.status(200).json(pets);
   } catch (e) {
     res.status(500).json(e);
   }
-})
-
+});
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const { User } = require('../../models');
-const bcrypt = require('bcrypt');
+const router = require("express").Router();
+const { User } = require("../../modelsxx");
+const bcrypt = require("bcrypt");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => { // Create new account registration
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create(req.body); // create new user
 
-    req.session.save(() => {
+    req.session.save(() => { // save session
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      req.session.admin = userData.admin;
+      req.session.admin = userData.admin; // save admin status for display settings
 
       res.status(200).json(userData);
     });
@@ -18,43 +18,45 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => { // login route
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-    if (!userData) {
+    const userData = await User.findOne({ where: { email: req.body.email } }); // find user if email exists
+    if (!userData) { // if no user is found, send error message
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
-    const validPassword = await bcrypt.compareSync(req.body.password, userData.password);
+    const validPassword = await bcrypt.compareSync( // Compare password validation
+      req.body.password,
+      userData.password
+    );
 
-    if (!validPassword) {
+    if (!validPassword) { // if password invalid, send error
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
-    req.session.save(() => {
+    req.session.save(() => { // save session
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       req.session.admin = userData.admin;
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.json({ user: userData, message: "You are now logged in!" });
     });
-
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+router.post("/logout", (req, res) => { // logout route
+  if (req.session.logged_in) { // destroy session if it exists
     req.session.destroy(() => {
       res.status(204).end();
-      res.render('/login');
+      res.render("/login");
     });
-  } else {
+  } else { // otherwise return error, not logged in
     res.status(404).end();
   }
 });
